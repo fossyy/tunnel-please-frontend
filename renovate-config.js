662f76281b5c335f6ae32c58ed1d0ac630b7950d
bagas <bagas@fossy.my.id>
2025-12-30 14:54:26 +0700

    feat: add renovate

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "endpoint": "https://git.fossy.my.id/api/v1",
+    "gitAuthor": "Renovate-Clanker <renovate-bot@fossy.my.id>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json",
+    "autodiscover": true,
+    "optimizeForDisabled": true,
+};
